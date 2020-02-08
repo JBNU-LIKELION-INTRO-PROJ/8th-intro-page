c73a3d7a147116c729364d2a7e10d2626f9319f3
caesiumy <mn0658@naver.com>
2020-02-08 22:33:11 +0900

    chore: Deploy to gh-pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,92 +4,97 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
 
-const postcssPlugins = [
-  tailwind(),
-]
+const postcssPlugins = [tailwind()];
 
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
 
 module.exports = {
-  siteName: 'Gridsome Portfolio Starter',
-  siteDescription: 'A simple portfolio theme for Gridsome powered by Tailwind CSS v1',
-  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  siteName: "JBNU Like Lion 8th Intro Page",
+  siteDescription: "JBNU Like Lion 8th Intro Page",
+  siteUrl: "https://jbnu-likelion-intro-proj.github.io/8th-intro-page/",
+  pathPrefix: "/8th-intro-page/",
   plugins: [
     {
-      use: '@gridsome/vue-remark',
+      use: "@gridsome/vue-remark",
       options: {
-        typeName: 'Documentation', // Required
-        baseDir: './docs', // Where .md files are located
-        pathPrefix: '/docs', // Add route prefix. Optional
-        template: './src/templates/Documentation.vue', // Optional
+        typeName: "Documentation", // Required
+        baseDir: "./docs", // Where .md files are located
+        pathPrefix: "/docs", // Add route prefix. Optional
+        template: "./src/templates/Documentation.vue", // Optional
         plugins: [
-          [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
-      ],
+          [
+            "gridsome-plugin-remark-shiki",
+            { theme: "Material-Theme-Palenight", skipInline: true }
+          ]
+        ]
       }
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'blog/**/*.md',
-        typeName: 'Post',
+        path: "blog/**/*.md",
+        typeName: "Post",
         refs: {
           tags: {
-            typeName: 'Tag',
+            typeName: "Tag",
             create: true
           }
         }
       }
     },
     {
-      use: 'gridsome-plugin-rss',
+      use: "gridsome-plugin-rss",
       options: {
-        contentTypeName: 'Post',
+        contentTypeName: "Post",
         feedOptions: {
-          title: 'Gridsome Portfolio Starter Blog',
-          feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
-          site_url: 'https://gridsome-portfolio-starter.netlify.com/'
+          title: "Gridsome Portfolio Starter Blog",
+          feed_url: "https://gridsome-portfolio-starter.netlify.com/rss.xml",
+          site_url: "https://gridsome-portfolio-starter.netlify.com/"
         },
         feedItemOptions: node => ({
           title: node.title,
           description: node.summary,
-          url: 'https://gridsome-portfolio-starter.netlify.com' + node.path,
-          author: 'Andre Madarang',
+          url: "https://gridsome-portfolio-starter.netlify.com" + node.path,
+          author: "Andre Madarang",
           date: node.date
         }),
         output: {
-          dir: './static',
-          name: 'rss.xml'
+          dir: "./static",
+          name: "rss.xml"
         }
       }
     },
     {
-      use: '@gridsome/plugin-sitemap',
+      use: "@gridsome/plugin-sitemap",
       options: {
-        cacheTime: 600000, // default
+        cacheTime: 600000 // default
       }
-    },
+    }
   ],
   templates: {
-    Tag: '/tag/:id'
+    Tag: "/tag/:id"
   },
   transformers: {
     remark: {
       plugins: [
-        [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+        [
+          "gridsome-plugin-remark-shiki",
+          { theme: "Material-Theme-Palenight", skipInline: true }
+        ]
       ],
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link"
     }
   },
   css: {
     loaderOptions: {
       postcss: {
-        plugins: postcssPlugins,
-      },
-    },
-  },
-}
+        plugins: postcssPlugins
+      }
+    }
+  }
+};
