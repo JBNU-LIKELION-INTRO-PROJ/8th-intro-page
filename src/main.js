59638631241f18e63bc09563b56f253fb57db925
caesiumy <mn0658@naver.com>
2020-02-10 22:34:24 +0900

    style: Change font family to Noto Sans KR & Change meta tags

--- src/main.js
+++ src/main.js
@@ -1,40 +1,40 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import VueScrollTo from 'vue-scrollto'
-import VueFuse from 'vue-fuse'
+import DefaultLayout from "~/layouts/Default.vue";
+import VueScrollTo from "vue-scrollto";
+import VueFuse from "vue-fuse";
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
 
   Vue.use(VueScrollTo, {
-    duration: 500,
-    easing: "ease",
-  })
+    duration: 1000,
+    easing: "ease"
+  });
 
-  Vue.use(VueFuse)
-
-  head.meta.push({
-    name: 'keywords',
-    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
-  })
+  Vue.use(VueFuse);
 
   head.meta.push({
-    name: 'description',
-    content: 'Gridsome Portfolio Starter'
-  })
+    name: "keywords",
+    content:
+      "Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS"
+  });
 
   head.meta.push({
-    name: 'author',
-    content: 'Andre Madarang'
-  })
+    name: "description",
+    content: "JBNU Like Lion 8th Intro Page"
+  });
+
+  head.meta.push({
+    name: "author",
+    content: "Like Lion 8th Managers"
+  });
 
   head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700'
-  })
+    rel: "stylesheet",
+    href:
+      "https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean"
+  });
 }
-
-
