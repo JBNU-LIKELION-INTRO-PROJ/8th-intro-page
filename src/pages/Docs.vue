376de2d47a8f112aae94005eae14ceae91880dbf
PeterPPark <petepp@naver.com>
2020-02-17 17:48:17 +0900

    complete profile with mobile

--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -1,23 +1,88 @@
 <template>
   <Layout>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <div class="max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-auto">
-    <img class="w-screen" src="../../blog/images/1601s.jpg" alt="profile of jhp">
-    <div class="px-4 py-2 mb-1">
-      <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">박종현</div>
-      <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 대표 &bull;기획 담당 </p>
-    </div>
-    <div class="px-4 py-2 flex">
-      <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
-      <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
-      <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
-      <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
-    </div>
-      
+  <div class="cards flex flex-wrap">
     
-</div>
+    <div class="card1 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto ">
+      <img class="w-screen h-64" src="../../blog/images/1601s.jpg" alt="profile of jhp">
+      <div class="px-4 py-2 mb-1">
+        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">박종현</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 대표 &bull;기획 담당 </p>
+      </div>
+      <div class="px-4 py-2 flex">
+        <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
+        <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+        <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
+      </div>
+    </div>
 
+    <div class="card2 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="https://images.wsj.net/im-132147?width=1280&size=1" alt="profile of gkm">
+      <div class="px-4 py-2 mb-1">
+        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">구경민</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 부대표 &bull;데이터 담당 </p>
+      </div>
+      <div class="px-4 py-2 flex">
+        <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
+        <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+        <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
+      </div>
+    </div>
+    <div class="card3 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="https://pds.joins.com/news/component/htmlphoto_mmdata/201910/28/htm_20191028171651842041.jpg" alt="profile of lbk">
+      <div class="px-4 py-2 mb-1">
+        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">이범기</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 운영진 &bull;예산 담당 </p>
+      </div>
+      <div class="px-4 py-2 flex">
+        <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
+        <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+        <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
+      </div>
+    </div>
+    <div class="card4 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="https://pds.joins.com/news/component/htmlphoto_mmdata/202001/30/c7fab409-d2a5-4568-b390-814376820fb8.jpg" alt="profile of khh">
+      <div class="px-4 py-2 mb-1">
+        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">김현희</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 운영진 &bull;개발 담당 </p>
+      </div>
+      <div class="px-4 py-2 flex">
+        <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
+        <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+        <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
+      </div>
+    </div>
+    <div class="card5 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="https://img.hani.co.kr/imgdb/resize/2017/0914/150523852061_20170914.JPG" alt="profile of jjh">
+      <div class="px-4 py-2 mb-1">
+        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">장준혁</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 운영진 &bull;홍보 담당 </p>
+      </div>
+      <div class="px-4 py-2 flex">
+        <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
+        <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+        <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
+      </div>
+    </div>
+    <div class="card6 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="https://avatars2.githubusercontent.com/u/35517199?s=460&v=4" alt="profile of ycs">
+      <div class="px-4 py-2 mb-1">
+        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">윤창식</div>
+        <p class="text-gray-700 text-xs tracking-wide ">7기 멋사 대표 &bull;개발 담당 </p>
+      </div>
+      <div class="px-4 py-2 flex">
+        <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
+        <p class="text-xs mx-1"><a href="https://github.com/PeterPPark">brunch</a></p>
+        <svg class="h-5 w-5 mx-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.93 17.07A10 10 0 1117.07 2.93 10 10 0 012.93 17.07zm12.73-1.41A8 8 0 104.34 4.34a8 8 0 0011.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+        <p class="text-xs"><a href="https://www.notion.so/jbnulikelion8/8-notion-6bdc700dfc3b43199dfaf1a8987f90a4">information</a></p>
+      </div>
+    </div>
+  </div>
+</div>  
   </Layout>
 </template>
 
