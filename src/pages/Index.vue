13bb47d138939fa99b51e722593e2eded0252baf
caesiumy <mn0658@naver.com>
2020-02-17 21:52:03 +0900

    style: Update section 2-2 bg position

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -58,7 +58,7 @@
         <div class="lg:flex lg:flex-row-reverse lg:mt-20">
           <!-- 2-2 Section bg pic -->
           <div
-            class="lg:w-60vw xl:h-35vh h-30vh bg-no-repeat bg-center bg-cover w-full xl:bg-bottom-m13"
+            class="lg:w-60vw xl:h-35vh h-30vh bg-no-repeat bg-center bg-cover w-full xl:bg-bottom-m12"
             style="background-image: url('https://raw.githubusercontent.com/CaesiumY/8th-intro-page/master/static/section2_pic2.jpg')"
           ></div>
           <!-- 2-2 Section text -->
