2605327b2607911229837c3b5033117579fa06a5
PeterPPark <petepp@naver.com>
2020-02-18 14:33:34 +0900

    modified members

--- src/pages/Docs.vue
+++ src/pages/Docs.vue
@@ -1,12 +1,12 @@
 <template>
   <Layout>
-  <div class="cards flex flex-wrap">
+  <div class="cards flex flex-wrap bg-center bg-cover text-center">
     
-    <div class="card1 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto ">
-      <img class="w-screen h-64" src="../../blog/images/1601s.jpg" alt="profile of jhp">
+    <div class="card1 max-w-xs rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="../../static/1601s.jpg" alt="profile of jhp">
       <div class="px-4 py-2 mb-1">
-        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">박종현</div>
-        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 대표 &bull;기획 담당 </p>
+        <div class="font-semibold text-xl font-sans text-lg mb-1 text-orange-300">박종현</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 대표 &bull;기획 담당 </p>
       </div>
       <div class="px-4 py-2 flex">
         <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
@@ -16,11 +16,11 @@
       </div>
     </div>
 
-    <div class="card2 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
-      <img class="w-screen h-64" src="https://images.wsj.net/im-132147?width=1280&size=1" alt="profile of gkm">
+    <div class="card2 max-w-xs rounded overflow-hidden shadow-xl m-6 flex-auto">
+      <img class="w-screen h-64" src="../../static/kkm.jpg" alt="profile of gkm">
       <div class="px-4 py-2 mb-1">
-        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">구경민</div>
-        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 부대표 &bull;데이터 담당 </p>
+        <div class="font-semibold text-xl font-sans text-lg mb-1 text-orange-300">구경민</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 부대표 &bull;데이터 담당 </p>
       </div>
       <div class="px-4 py-2 flex">
         <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
@@ -30,10 +30,10 @@
       </div>
     </div>
     <div class="card3 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
-      <img class="w-screen h-64" src="https://pds.joins.com/news/component/htmlphoto_mmdata/201910/28/htm_20191028171651842041.jpg" alt="profile of lbk">
+      <img class="w-screen h-64" src="https://image.chosun.com/sitedata/image/201812/04/2018120400079_0.jpg" alt="profile of lbk">
       <div class="px-4 py-2 mb-1">
-        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">이범기</div>
-        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 운영진 &bull;예산 담당 </p>
+        <div class="font-semibold text-xl font-sans text-lg mb-1 text-orange-300">이범기</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 운영진 &bull;예산 담당 </p>
       </div>
       <div class="px-4 py-2 flex">
         <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
@@ -43,10 +43,10 @@
       </div>
     </div>
     <div class="card4 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
-      <img class="w-screen h-64" src="https://pds.joins.com/news/component/htmlphoto_mmdata/202001/30/c7fab409-d2a5-4568-b390-814376820fb8.jpg" alt="profile of khh">
+      <img class="w-screen h-64" src="https://lh3.googleusercontent.com/proxy/cbkvZYt-EfLvFJi0YYM3ONxtCWW_41Pkwt6C_PCeLQ15fhPYCUl7Kpa3q8T9B4xHTUTXoucdWUyXwY2R27K79MkrSVSYCEh0DQre6dzRfrefhlfqHZbOtx2as_WxMK35fpVF-6Q" alt="profile of khh">
       <div class="px-4 py-2 mb-1">
-        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">김현희</div>
-        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 운영진 &bull;개발 담당 </p>
+        <div class="font-semibold text-xl font-sans-Cambria text-lg mb-1 text-orange-300">김현희</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 운영진 &bull;개발 담당 </p>
       </div>
       <div class="px-4 py-2 flex">
         <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
@@ -58,8 +58,8 @@
     <div class="card5 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
       <img class="w-screen h-64" src="https://img.hani.co.kr/imgdb/resize/2017/0914/150523852061_20170914.JPG" alt="profile of jjh">
       <div class="px-4 py-2 mb-1">
-        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">장준혁</div>
-        <p class="text-gray-700 text-xs tracking-wide ">8기 멋사 운영진 &bull;홍보 담당 </p>
+        <div class="font-semibold text-xl font-sans text-lg mb-1 text-orange-300">장준혁</div>
+        <p class="text-gray-700 text-xs tracking-wide ">8기 운영진 &bull;홍보 담당 </p>
       </div>
       <div class="px-4 py-2 flex">
         <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
@@ -71,8 +71,8 @@
     <div class="card6 max-w-xs w-1/2 rounded overflow-hidden shadow-xl m-6 flex-auto">
       <img class="w-screen h-64" src="https://avatars2.githubusercontent.com/u/35517199?s=460&v=4" alt="profile of ycs">
       <div class="px-4 py-2 mb-1">
-        <div class="font-semibold text-xl font-serif-Cambria text-lg mb-1 text-blue-400">윤창식</div>
-        <p class="text-gray-700 text-xs tracking-wide ">7기 멋사 대표 &bull;개발 담당 </p>
+        <div class="font-semibold text-xl font-sans text-lg mb-1 text-orange-300">윤창식</div>
+        <p class="text-gray-700 text-xs tracking-wide ">7기 대표 &bull;개발 담당 </p>
       </div>
       <div class="px-4 py-2 flex">
         <svg class="h-5 w-5 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 27"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg>
